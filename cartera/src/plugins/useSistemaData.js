1d77439bfe77a9ce1e2d148cd62c878287c6dae2
BastianCarrasco <88857266+BastianCarrasco@users.noreply.github.com>
2025-06-10 11:08:32 -0400

    ajuste url

--- cartera/src/plugins/useSistemaData.js
+++ cartera/src/plugins/useSistemaData.js
@@ -6,7 +6,7 @@ const tematicaUrl = import.meta.env.VITE_API_URL_ALLTEMATICAS;
 const convocatoriasUrl = import.meta.env.VITE_API_URL_CONVOCATORIAS;
 const academicosUrl= import.meta.env.VITE_API_URL_ACADEMICOS; 
 const UAURL = import.meta.env.VITE_API_URL_UA;
-const proyectosUrl = import.meta.env.VITE_API_URL_PROYECTOS;
+const proyectosUrl = import.meta.env.VITE_API_URL_AllPROYECTOS
 const apoyos_tags = import.meta.env.VITE_API_URL_TAGS;
 
 export async function fetchTags() {
